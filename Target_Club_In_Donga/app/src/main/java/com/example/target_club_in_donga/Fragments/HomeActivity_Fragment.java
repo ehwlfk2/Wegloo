9f4d0087aa220d42a1c411e628d061e3856355a3
hyni24 <hyni24@gmail.com>
2019-09-20 11:23:29 +0900

    가계부 파업

--- Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/Fragments/HomeActivity_Fragment.java
+++ Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/Fragments/HomeActivity_Fragment.java
@@ -15,7 +15,6 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.target_club_in_donga.Accountbook.AccountbookActivity_Main;
 import com.example.target_club_in_donga.AttendActivity;
 import com.example.target_club_in_donga.History.HistoryActivity_Main;
 import com.example.target_club_in_donga.HomeActivity;
@@ -199,10 +198,9 @@ public class HomeActivity_Fragment extends Fragment {
         main_btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getActivity(), AccountbookActivity_Main.class);
-                startActivity(intent);
+
             }
-        });
+        }); //가계부 파업중
         main_btn_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
