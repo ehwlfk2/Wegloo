8aa59d25f8ab88af16a8913061d65c2f9e6bf8e2
HanDongWoon <gkdlehddns@gmail.com>
2019-09-20 16:49:28 +0900

    Delete Accountbook
    가계부 삭제

--- Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/Accountbook/Accountbook_Item2.java
+++
@@ -1,9 +0,0 @@
-package com.example.target_club_in_donga.Accountbook;
-
-
-public class Accountbook_Item2 {
-    public String id;
-    public int price;
-    public String detail;
-    public Object today;
-}
