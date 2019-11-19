dcbcad27e9993ed2f686db3e57815e3ec007a8c6
hyni24 <hyni24@gmail.com>
2019-11-19 16:31:16 +0900

    아악

--- Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/home_viewpager/MyInformation.java
+++ Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/home_viewpager/MyInformation.java
@@ -1,10 +1,13 @@
-package com.example.target_club_in_donga;
+package com.example.target_club_in_donga.home_viewpager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.target_club_in_donga.R;
+
 public class MyInformation extends AppCompatActivity {
     private ImageButton btn_back;
     @Override
