cd45044c9eb55876f70d352d806fd511597e16e4
ehwlfk2 <ehwlfk2@gmail.com>
2019-11-26 17:01:05 +0900

    updateCalenar

--- Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/calendar/room/Todo.java
+++ Target_Club_In_Donga/app/src/main/java/com/example/target_club_in_donga/calendar/room/Todo.java
@@ -14,7 +14,10 @@ public class Todo {
     private String title;
 
     // Constructor
-    public Todo(String title, boolean isChecked) { this.title = title; this.isChecked = isChecked; }
+    public Todo(String title, boolean isChecked) {
+        this.title = title;
+        this.isChecked = isChecked;
+    }
 
     // 내용을 확인할 수 있도록 재정의
     @Override
@@ -27,14 +30,29 @@ public class Todo {
     }
 
     // ID getter, setter
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     // isCehcked getter, setter
-    public boolean getIsChecked() { return isChecked; }
-    public void setIsChecked(boolean isChecked) { this.isChecked = isChecked; }
+    public boolean getIsChecked() {
+        return isChecked;
+    }
+
+    public void setIsChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+    }
 
     // title getter, setter
-    public String getTitle() {return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
